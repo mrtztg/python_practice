ca641d26dc3ab77c693e905d1b7f2664efffe22e
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-07-13 20:17:46 +0100

    solved findMissingNumber

--- tests/test_ch001_find_missing_number.py
+++ tests/test_ch001_find_missing_number.py
@@ -1,8 +1,16 @@
-import unittest
+# import unittest
+from challenges.ch001_find_missing_number import ch001_find_missing_number
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)  # add assertion here
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+#
+
+def test_find_missing_number():
+    assert ch001_find_missing_number([4,0,3,1]) == 2
+    assert ch001_find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]) == 7
